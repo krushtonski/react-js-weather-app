d8414c9066df0ae11d38df816ad6bb545c63df52
krushtonski <36168072+krushtonski@users.noreply.github.com>
2021-02-28 16:58:50 +0000

    corrections

--- src/Weatherinfo.js
+++ src/Weatherinfo.js
@@ -24,7 +24,7 @@ export default function WeatherInfo(props){
         </div>
         </div>
     </div>
-    <div className="col-sm-2 col-md-3">
+    <div className="col-sm-4 col-md-3">
         <div className="float-left current-main-temp">
         <WeatherTemperature celsius={props.data.temperature} />
         </div>
