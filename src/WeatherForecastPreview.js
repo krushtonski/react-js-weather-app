56dfb954242c2666b89f9ee3c0e91713ad4550ef
krushtonski <36168072+krushtonski@users.noreply.github.com>
2021-02-28 18:11:27 +0000

    amends

--- src/WeatherForecastPreview.js
+++ src/WeatherForecastPreview.js
@@ -11,7 +11,7 @@ export default function WeatherForecastPreview(props){
   }
     return (
       <div>
-      <div className="WeatherForecastPrev col">
+      <div className="WeatherForecastPrev">
        {formatDay()}
       <div className="forecasticon"><WeatherIcon code={props.data.weather[0].icon}/></div>
            <span className="temperatures"><strong>{Math.round(props.data.temp.min)}</strong>°<span className="min-temps"><span className="divider">|</span>{Math.round(props.data.temp.max)}° </span></span>
