7244e9ae6fb51b35f5fb0e53b7adbb4231ac3c4b
krushtonski <36168072+krushtonski@users.noreply.github.com>
2021-02-28 10:31:37 +0000

    corrections

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -22,10 +22,10 @@ if (loaded && props.lat === forecastData.lat){
 return (
    <div className = "row">
     <div className="WeatherForecast row">
-      <div  className="col-sm-12 col-m-6"><WeatherForecastPreview data={forecastData.daily[0]} /></div>
-      <div className="col-sm-12 col-m-6"><WeatherForecastPreview data={forecastData.daily[1]} /></div>
-      <div className="col-sm-12 col-m-6"><WeatherForecastPreview data={forecastData.daily[2]} /></div>
-      <div className="col-sm-12 col-m-6"><WeatherForecastPreview data={forecastData.daily[3]} /></div>
+      <div  className="col-sm-12 col-md-3"><WeatherForecastPreview data={forecastData.daily[0]} /></div>
+      <div className="col-sm-12 col-md-3"><WeatherForecastPreview data={forecastData.daily[1]} /></div>
+      <div className="col-sm-12 col-md-3"><WeatherForecastPreview data={forecastData.daily[2]} /></div>
+      <div className="col-sm-12 col-md-3"><WeatherForecastPreview data={forecastData.daily[3]} /></div>
     </div>
     </div>
 );
