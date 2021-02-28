c1fe4f2a4e0713d22a8be39ec1889b9d3b5e5a9e
krushtonski <36168072+krushtonski@users.noreply.github.com>
2021-02-28 12:58:14 +0000

    centering weather icon for forecast

--- src/WeatherForecastPreview.js
+++ src/WeatherForecastPreview.js
@@ -12,7 +12,7 @@ export default function WeatherForecastPreview(props){
     return (
      <div className="WeatherForecastPrev col">
        {formatDay()}
-       <WeatherIcon code={props.data.weather[0].icon}/>
+      <div className="forecast-icon"> <WeatherIcon code={props.data.weather[0].icon}/></div>
            <span className="temperatures"><strong>{Math.round(props.data.temp.min)}</strong>°<span className="min-temps"><span className="divider">|</span>{Math.round(props.data.temp.max)}° </span></span>
       </div>
     );
