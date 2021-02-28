1fb13060ed337c22e90c103067bc12106674c9c5
krushtonski <36168072+krushtonski@users.noreply.github.com>
2021-02-28 14:45:02 +0000

    corrections

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -22,12 +22,12 @@ if (loaded && props.lat === forecastData.lat){
 return (
    <div className = "row">
     <div className="WeatherForecast row">
-      <div className="col-sm-6 col-md-2 forecast-individual"><WeatherForecastPreview data={forecastData.daily[0]} /></div>
-      <div className="col-sm-6 col-md-2 forecast-individual"><WeatherForecastPreview data={forecastData.daily[1]} /></div>
-      <div className="col-sm-6 col-md-2 forecast-individual"><WeatherForecastPreview data={forecastData.daily[2]} /></div>
-      <div className="col-sm-6 col-md-2 forecast-individual"><WeatherForecastPreview data={forecastData.daily[3]} /></div>
-      <div className="col-sm-6 col-md-2 forecast-individual"><WeatherForecastPreview data={forecastData.daily[4]} /></div>
-      <div className="col-sm-6 col-md-2 forecast-individual"><WeatherForecastPreview data={forecastData.daily[5]} /></div>
+      <div className="col-sm-6 col-md-2"><WeatherForecastPreview data={forecastData.daily[0]} /></div>
+      <div className="col-sm-6 col-md-2"><WeatherForecastPreview data={forecastData.daily[1]} /></div>
+      <div className="col-sm-6 col-md-2"><WeatherForecastPreview data={forecastData.daily[2]} /></div>
+      <div className="col-sm-6 col-md-2"><WeatherForecastPreview data={forecastData.daily[3]} /></div>
+      <div className="col-sm-6 col-md-2"><WeatherForecastPreview data={forecastData.daily[4]} /></div>
+      <div className="col-sm-6 col-md-2"><WeatherForecastPreview data={forecastData.daily[5]} /></div>
       </div>
     </div>
 );
