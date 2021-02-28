99833f927ee7fed614d8838edc34297197c0aea1
krushtonski <36168072+krushtonski@users.noreply.github.com>
2021-02-28 14:42:01 +0000

    changing css for weather icon

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactAnimatedWeather from "react-animated-weather";
-import "./WeatherIcon.css";
 
 export default function WeatherIcon(props){
     const codeMapping = {
